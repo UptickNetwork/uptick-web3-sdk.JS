6fd62d899d5e9d9a9dfdb13591b48b4b62220f6c
Wangxiaolei123456 <897672818@qq.com>
2024-12-20 11:21:09 +0800

    replace the meatamask conncect methods

--- src/wallet.js
+++ src/wallet.js
@@ -1,26 +1,39 @@
-const {utils} = require('ethers')
+const { utils } = require('ethers');
 
 const Web3 = require('web3');
 
 export const getMatamaskWeb3 = async () => {
-    var web3;
-    if (window.ethereum) {
-        // Modern dapp browsers
-        web3 = new Web3(window.ethereum);
-        try {
-            await window.ethereum.enable();
-            await window.ethereum.eth_requestAccounts();
-        } catch (error) {
-            console.log(error);
-        }
-    } else {
+  var web3;
+  if (window.ethereum) {
+    // Modern dapp browsers
+    web3 = new Web3(window.ethereum);
+    try {
+      await window.ethereum.enable();
+      await window.ethereum.eth_requestAccounts();
+    } catch (error) {
+      console.log(error);
     }
-    let accounts = await web3.eth.getAccounts();
-    return accounts;
+  } else {
+  }
+  let accounts = await web3.eth.getAccounts();
+  return accounts;
 };
 
 export const isMatamasklogin = async () => {
-    if (window.ethereum.isConnected()) {
-        getMatamaskWeb3();
-    }
+  if (await checkConnection()) {
+    getMatamaskWeb3();
+  }
 };
+
+async function checkConnection() {
+  try {
+    const accounts = await window.ethereum.request({
+      method: 'eth_accounts',
+    });
+    console.log('connect success');
+    return accounts.length > 0;
+  } catch (error) {
+    console.error('connect success', error);
+    return false;
+  }
+}
