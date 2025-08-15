57dd7ed8cfe1fe5976d24fee6606c229260089d5
Wangxiaolei123456 <897672818@qq.com>
2025-08-15 18:50:18 +0800

    update erc1155 js file

--- src/evm/handler/erc1155Auction.js
+++ src/evm/handler/erc1155Auction.js
@@ -1,24 +1,20 @@
 import {
-    connect,
-    wallectConnectSendTransaction,
-    isWalletConnect
-} from "./common";
+  connect,
+  wallectConnectSendTransaction,
+  isWalletConnect,
+} from './common';
 
-import {
-    abi
-} from "../abi/ERC1155Auction.json";
-import {utils} from "ethers";
+import { abi } from '../abi/ERC1155Auction.json';
+import { utils } from 'ethers';
 
 const base = require('./base');
 
-
-let contractAddress = "0x99a415da5b4d061556e2d55c3382cfeda02a5a7d"
+let contractAddress = '0x99a415da5b4d061556e2d55c3382cfeda02a5a7d';
 
 export function setContractAddress(platformAddress) {
-
-    if (platformAddress) {
-        contractAddress = platformAddress;
-    }
+  if (platformAddress) {
+    contractAddress = platformAddress;
+  }
 }
 
 // address _owner,
@@ -28,214 +24,289 @@ export function setContractAddress(platformAddress) {
 // address _to,
 // bytes calldata data
 export async function transfer(owner, tokenId, value, assetId) {
-    const account = await base.getAccounts();
-    const address = await account.getAddress();
+  const account = await base.getAccounts();
+  const address = await account.getAddress();
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-    let data = JSON.stringify({assetId: [assetId]});
-    let amount = 1;
+  let data = JSON.stringify({ assetId: [assetId] });
+  let amount = 1;
 
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
 
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-
-        let rep = await contract.transfer(owner, tokenId, value, amount, address, utils.toUtf8Bytes(data), {
-            value: value, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-        return rep;
-
-
-    } else {
-        let data = contract.methods.transfer(owner, tokenId, value, amount, address, utils.toUtf8Bytes(data)).encodeABI()
-        let result = await wallectConnectSendTransaction(address, contractAddress, data, value);
-        return result;
-
-    }
-
-
+    let rep = await contract.transfer(
+      owner,
+      tokenId,
+      value,
+      amount,
+      address,
+      utils.toUtf8Bytes(data),
+      {
+        value: value,
+        gasPrice: gasSetting.gasPrice,
+        gasLimit: gasSetting.gasLimit,
+      }
+    );
+    return rep;
+  } else {
+    let data = contract.methods
+      .transfer(owner, tokenId, value, amount, address, utils.toUtf8Bytes(data))
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      address,
+      contractAddress,
+      data,
+      value
+    );
+    return result;
+  }
 }
 
-export async function onSale(nftAddress, nftid, startTimeStamp, endTimeStamp, startBid, fixPrice, ReserveBid, fee, amount, payAddress) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+export async function onSale(
+  nftAddress,
+  nftid,
+  startTimeStamp,
+  endTimeStamp,
+  startBid,
+  fixPrice,
+  ReserveBid,
+  fee,
+  amount,
+  payAddress
+) {
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
 
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.createAuction(
+      nftAddress,
+      nftid,
+      amount,
+      startTimeStamp,
+      endTimeStamp,
+      startBid,
+      fixPrice,
+      ReserveBid,
+      payAddress,
+      {
+        value: fee,
+        gasPrice: gasSetting.gasPrice,
+        gasLimit: gasSetting.gasLimit,
+      }
+    );
 
-        let rep = await contract.createAuction(nftAddress, nftid, amount, startTimeStamp, endTimeStamp, startBid, fixPrice, ReserveBid, payAddress, {
-            value: fee, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-
-        return rep;
-
-    } else {
-        let data = contract.methods.createAuction(nftAddress, nftid, amount, startTimeStamp, endTimeStamp, startBid, fixPrice, ReserveBid, payAddress).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, fee);
-        return result;
-
-    }
-
-
+    return rep;
+  } else {
+    let data = contract.methods
+      .createAuction(
+        nftAddress,
+        nftid,
+        amount,
+        startTimeStamp,
+        endTimeStamp,
+        startBid,
+        fixPrice,
+        ReserveBid,
+        payAddress
+      )
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      fee
+    );
+    return result;
+  }
 }
-
 
 export async function placeBid(nftAddress, nftid, fixPrice, owner, fee) {
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
 
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+    let rep = await contract.bid(nftAddress, owner, nftid, fixPrice, {
+      value: fee,
+      gasPrice: gasSetting.gasPrice,
+      gasLimit: gasSetting.gasLimit,
+    });
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-
-        let rep = await contract.bid(nftAddress, owner, nftid, fixPrice, {
-            value: fee, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-
-        return rep;
-
-
-    } else {
-        let data = contract.methods.bid(nftAddress, owner, nftid, fixPrice).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, fixPrice);
-        return result;
-
-    }
-
-
+    return rep;
+  } else {
+    let data = contract.methods
+      .bid(nftAddress, owner, nftid, fixPrice)
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      fixPrice
+    );
+    return result;
+  }
 }
 
-
 export async function end(nftAddress, nftid, owner) {
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
 
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
 
+    let rep = await contract.endAuction(nftAddress, owner, nftid, {
+      gasPrice: gasSetting.gasPrice,
+      gasLimit: gasSetting.gasLimit,
+    });
 
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-
-        let rep = await contract.endAuction(nftAddress, owner, nftid, {
-            gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-
-        return rep;
-
-    } else {
-        let data = contract.methods.endAuction(nftAddress, owner, nftid).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, "0");
-        return result;
-
-    }
-
-
+    return rep;
+  } else {
+    let data = contract.methods
+      .endAuction(nftAddress, owner, nftid)
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      '0'
+    );
+    return result;
+  }
 }
 
 export async function placeOrder(nftAddress, nftId, toAddress, price) {
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
-
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let result = await contract.placeOrder(
-            nftAddress, nftId, toAddress, 1, fromAddress, utils.toUtf8Bytes(''),
-            {value: price, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit}
-        );
-        return result;
-
-
-    } else {
-        let data = contract.methods.placeOrder(nftAddress, nftId, toAddress, 1, fromAddress, utils.toUtf8Bytes('')).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, price);
-        return result;
-
-    }
-
-
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let result = await contract.placeOrder(
+      nftAddress,
+      nftId,
+      toAddress,
+      1,
+      fromAddress,
+      utils.toUtf8Bytes(''),
+      {
+        value: price,
+        gasPrice: gasSetting.gasPrice,
+        gasLimit: gasSetting.gasLimit,
+      }
+    );
+    return result;
+  } else {
+    let data = contract.methods
+      .placeOrder(
+        nftAddress,
+        nftId,
+        toAddress,
+        1,
+        fromAddress,
+        utils.toUtf8Bytes('')
+      )
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      price
+    );
+    return result;
+  }
 }
 
 export async function offSale(nftAddress, nftid) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let rep = await contract.offSale(nftAddress, nftid, fromAddress, {
-            gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-        return rep;
-
-    } else {
-        let data = contract.methods.offSale(nftAddress, nftid, fromAddress).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, "0");
-        return result;
-
-    }
-
-
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.offSale(nftAddress, nftid, fromAddress, {
+      gasPrice: gasSetting.gasPrice,
+      gasLimit: gasSetting.gasLimit,
+    });
+    return rep;
+  } else {
+    let data = contract.methods
+      .offSale(nftAddress, nftid, fromAddress)
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      '0'
+    );
+    return result;
+  }
 }
 
 export async function revokeApprove(tokenArr, onAssetIds, value) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
-    let data = JSON.stringify({assetId: onAssetIds});
-    let amount = onAssetIds.length;
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
+  let data = JSON.stringify({ assetId: onAssetIds });
+  let amount = onAssetIds.length;
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let rep = await contract.revokeApprove(tokenArr[0], value, amount, utils.toUtf8Bytes(data),
-            {gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit}
-        );
-        return rep;
-
-    } else {
-        let data = contract.methods.revokeApprove(tokenArr[0], value, amount, utils.toUtf8Bytes(data)).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, "0");
-        return result;
-
-    }
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.revokeApprove(
+      tokenArr[0],
+      value,
+      amount,
+      utils.toUtf8Bytes(data),
+      { gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit }
+    );
+    return rep;
+  } else {
+    let data = contract.methods
+      .revokeApprove(tokenArr[0], value, amount, utils.toUtf8Bytes(data))
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      '0'
+    );
+    return result;
+  }
 }
--- src/evm/handler/erc1155CouponPlatform.js
+++ src/evm/handler/erc1155CouponPlatform.js
@@ -1,140 +1,199 @@
 import {
-    connect,
-    connectCheck,
-    wallectConnectSendTransaction,
-    isWalletConnect
-} from "./common";
+  connect,
+  connectCheck,
+  wallectConnectSendTransaction,
+  isWalletConnect,
+} from './common';
 
-import {
-    abi
-} from "../abi/ERC1155CouponPlatform.json";
+import { abi } from '../abi/ERC1155CouponPlatform.json';
 
 const base = require('./base');
-import {utils} from "ethers";
+import { utils } from 'ethers';
 
-
-let contractAddress = "0xd3e379f75d08ba91f632b363f021ceda01d94984"
+let contractAddress = '0xd3e379f75d08ba91f632b363f021ceda01d94984';
 
 export function setContractAddress(platformAddress) {
-    if (platformAddress) {
-        contractAddress = platformAddress;
-    }
+  if (platformAddress) {
+    contractAddress = platformAddress;
+  }
 }
 
+export async function onSale(
+  nftAddress,
+  nftid,
+  value,
+  couponCode,
+  reducedPrice,
+  fee,
+  amount,
+  payAddress
+) {
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
 
-export async function onSale(nftAddress, nftid, value, couponCode, reducedPrice, fee, amount, payAddress) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
-
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let rep = await contract.onSale(nftAddress, nftid, value, amount, payAddress, couponCode, reducedPrice, {
-            value: fee, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-        return rep;
-
-
-    } else {
-        let data = contract.methods.onSale(nftAddress, nftid, value, amount, payAddress, couponCode, reducedPrice).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, fee);
-        return result;
-
-    }
-
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.onSale(
+      nftAddress,
+      nftid,
+      value,
+      amount,
+      payAddress,
+      couponCode,
+      reducedPrice,
+      {
+        value: fee,
+        gasPrice: gasSetting.gasPrice,
+        gasLimit: gasSetting.gasLimit,
+      }
+    );
+    return rep;
+  } else {
+    let data = contract.methods
+      .onSale(
+        nftAddress,
+        nftid,
+        value,
+        amount,
+        payAddress,
+        couponCode,
+        reducedPrice
+      )
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      fee
+    );
+    return result;
+  }
 }
 
 export async function offSale(nftAddress, nftid) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
 
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let rep = await contract.offSale(nftAddress, nftid, {
-            gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit
-        });
-        return rep;
-
-    } else {
-        let data = contract.methods.offSale(nftAddress, nftid).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, "0");
-        return result;
-
-    }
-
-
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.offSale(nftAddress, nftid, {
+      gasPrice: gasSetting.gasPrice,
+      gasLimit: gasSetting.gasLimit,
+    });
+    return rep;
+  } else {
+    let data = contract.methods.offSale(nftAddress, nftid).encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      '0'
+    );
+    return result;
+  }
 }
 
 export async function revokeApprovesWithArray(tokenArr) {
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let rep = await contract.revokeApprovesWithArray(tokenArr,
-            {gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit}
-        );
-        return rep;
-
-
-    } else {
-        let data = contract.methods.revokeApprovesWithArray(tokenArr).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, "0");
-        return result;
-
-    }
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let rep = await contract.revokeApprovesWithArray(tokenArr, {
+      gasPrice: gasSetting.gasPrice,
+      gasLimit: gasSetting.gasLimit,
+    });
+    return rep;
+  } else {
+    let data = contract.methods.revokeApprovesWithArray(tokenArr).encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      '0'
+    );
+    return result;
+  }
 }
 
 //   preOrderRes.data.couponCode,
 //preOrderRes.data.couponLink
-export async function placeOrder(nftAddress, nftId, toAddress, price, couponCode, couponLink) {
-    if (couponCode == null) {
-        couponCode = 0
-    }
-    if (couponLink == null) {
-        couponLink = 0
-    }
-    const account = await base.getAccounts();
-    const fromAddress = await account.getAddress();
-    let contract
-    if (!contract) {
-        contract = await connect(contractAddress, abi, account);
-    }
+export async function placeOrder(
+  nftAddress,
+  nftId,
+  toAddress,
+  price,
+  couponCode,
+  couponLink
+) {
+  if (couponCode == null) {
+    couponCode = 0;
+  }
+  if (couponLink == null) {
+    couponLink = 0;
+  }
+  const account = await base.getAccounts();
+  const fromAddress = await account.getAddress();
+  let contract;
+  if (!contract) {
+    contract = await connect(contractAddress, abi, account);
+  }
 
-
-    let hasWalletConnect = isWalletConnect();
-    if (!hasWalletConnect) {
-        let gasSetting = await base.getGasPriceAndGasLimit();
-        let result = await contract.placeOrder(
-            nftAddress, nftId, toAddress, 1, fromAddress, couponCode, couponLink, utils.toUtf8Bytes(''),
-            {value: price, gasPrice: gasSetting.gasPrice, gasLimit: gasSetting.gasLimit}
-        );
-        return result;
-    } else {
-        let data = contract.methods.placeOrder(nftAddress, nftId, toAddress, 1, fromAddress, couponCode, couponLink, utils.toUtf8Bytes('')).encodeABI()
-        let result = await wallectConnectSendTransaction(fromAddress, contractAddress, data, price);
-        return result;
-
-    }
-
+  let hasWalletConnect = isWalletConnect();
+  if (!hasWalletConnect) {
+    let gasSetting = await base.getGasPriceAndGasLimit();
+    let result = await contract.placeOrder(
+      nftAddress,
+      nftId,
+      toAddress,
+      1,
+      fromAddress,
+      couponCode,
+      couponLink,
+      utils.toUtf8Bytes(''),
+      {
+        value: price,
+        gasPrice: gasSetting.gasPrice,
+        gasLimit: gasSetting.gasLimit,
+      }
+    );
+    return result;
+  } else {
+    let data = contract.methods
+      .placeOrder(
+        nftAddress,
+        nftId,
+        toAddress,
+        1,
+        fromAddress,
+        couponCode,
+        couponLink,
+        utils.toUtf8Bytes('')
+      )
+      .encodeABI();
+    let result = await wallectConnectSendTransaction(
+      fromAddress,
+      contractAddress,
+      data,
+      price
+    );
+    return result;
+  }
 }
-
